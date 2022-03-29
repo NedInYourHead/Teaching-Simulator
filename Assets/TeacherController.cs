e21f2f24f24e91226cdfd7cc398aeea67e06035f
NedInYourHead <nedcorney@gmail.com>
2022-03-29 16:57:15 +0100

    Added horizontal movement. Now prioritises the last direction you pressed to avoid diagonal movement

--- Assets/TeacherController.cs
+++ Assets/TeacherController.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class TeacherController : MonoBehaviour
 {
+    private string movePriority;
+
     private float vertical;
     [SerializeField] private float speed = 0.2f;
 
     private float horizontal;
+    
     void FixedUpdate()
     {
-        vertical = Input.GetAxis("Vertical");
 
+        // the reason this part is so complex is because it only lets you move in x OR y and prioritises the direction you're NOT holding.
+        // probably need to clean it up and make it more elegant
         horizontal = Input.GetAxis("Horizontal");
 
-        transform.position = transform.position + (transform.up * vertical * speed);
+        vertical = Input.GetAxis("Vertical");
+
+        if (horizontal == 0f)
+        {
+            movePriority = "x";
+        }
+        if (vertical == 0f)
+        {
+            movePriority = "y";
+        }
+
+        if (movePriority == "x" || vertical == 0f)
+        {
+            transform.position = transform.position + (transform.right * horizontal * speed);
+        }
+        if (movePriority == "y" || horizontal == 0f)
+        {
+            transform.position = transform.position + (transform.up * vertical * speed);
+        }
+
     }
 }
